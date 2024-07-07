e26303229c28888a7d481e2b202cd9f26089281b
chuminjae <choochootrain1130@gmail.com>
2024-07-07 23:31:41 +0900

    update

--- test.py
+++ test.py
@@ -1,13 +1,24 @@
 c = [[0] * 10 for _ in range(10)]
 for i in range(10):
     c[i]=list(map(int, input().split()))
-x = 0
-y = 0
-for i in range(0,10):
-    for j in range(0,10):
-        c[i][j] == 2
-        x = i
-        y = j
 i = 1
 j = 1
-print(i, j)
+while c[i][j] != 2:
+    if(c[i][j] == 0):
+        c[i][j] = 9
+    if((i == 9 and j == 9) or (c[i][j + 1] == 1)and c[i+1][j] == 1):
+        break
+    if(c[i][j + 1] != 1):
+        j += 1
+        if(c[i][j] == 2):
+            c[i][j] = 9
+            break
+    elif(c[i + 1][j] != 1):
+        i += 1
+        if(c[i][j] == 2):
+            c[i][j] = 9
+            break
+for i in range(10):
+    for j in range(10):
+        print(c[i][j], end = " ")
+    print("")
